99788a5584f824a68c173ce976545ae9d832605d
Matt Johnson <mjohnson541@gmail.com>
2018-10-15 22:48:14 -0400

    add tests for diffusivity calculators

---
+++ rms/Calculators/TestDiffusion.jl
@@ -0,0 +1,20 @@
+using Test
+using Unitful
+
+include("Diffusion.jl")
+include("../Constants.jl")
+
+#EmptyDiffusivity
+dif = EmptyDiffusivity()
+@test_throws ErrorException dif(T=400.0)
+
+#ConstantDiffusivity
+D = 4.0
+dif = ConstantDiffusivity(D)
+@test dif(T=400.0,mu=1e3) == D
+
+#StokesDiffusivity
+r = 0.47e-9
+dif = StokesDiffusivity(r)
+D = dif(T=500.0,mu=.891e-3)
+@test kB*500.0/(6*pi*.891e-3*D) ≈ r rtol=1e-3
