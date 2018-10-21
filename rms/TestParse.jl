ce4983ccd25bc971f1fcfb69279a7c94cbba73e5
Matt Johnson <mjohnson541@gmail.com>
2018-10-21 15:59:38 -0400

    add some very basic tests for input file parsing

---
+++ rms/TestParse.jl
@@ -0,0 +1,9 @@
+using Test
+include("Parse.jl")
+out = readinput("testing/superminimal.yml")
+r = out["gas"]["Reactions"][1]
+s = r.products[1]
+
+@test r.kinetics(1000.0) ≈ 25618.8 rtol=1e-4
+@test getEnthalpy(s.thermo,1000.0) ≈ 20677.0 rtol=1e-4
+@test getEnthalpy(out["gas"]["Species"][5].thermo,1000.0) ≈ 20677.2 rtol=1e-4
