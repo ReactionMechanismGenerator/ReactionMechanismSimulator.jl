a7a1bf373317772a2cb68359fc8f0436738fdea0
Matt Johnson <mjohnson541@gmail.com>
2018-10-23 16:34:04 -0400

    change the reaction object structure

--- rms/Reaction.jl
+++ rms/Reaction.jl
@@ -6,9 +6,12 @@ include("Species.jl")
 abstract type AbstractReaction end
 export AbstractReaction
 
-@with_kw struct Reaction{T<:AbstractRate,N<:AbstractSpecies} <: AbstractReaction
+@with_kw struct ElementaryReaction{T<:AbstractRate,N<:AbstractSpecies,Q<:Integer} <: AbstractReaction
+    index::Q
     reactants::Array{N,1}
+    reactantInds::Array{Q,1}
     products::Array{N,1}
+    productInds::Array{Q,1}
     kinetics::T
 end
-export Reaction
+export ElementaryReaction
