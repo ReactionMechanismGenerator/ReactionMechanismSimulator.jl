97d5650f0e3af4934a18829615af76ecf5d6d7a7
Matt Johnson <mjohnson541@gmail.com>
2018-10-28 21:12:32 -0400

    add calcthermo! methods for updating state variables for a given domain before calculating rates

--- rms/Domain.jl
+++ rms/Domain.jl
@@ -39,5 +39,37 @@ export ConstantVDomain
 end
 ConstantTVDomain(;state::T,phase::N,interfaces::Array{Q,1}=Array{EmptyInterface,1}()) where {T<:AbstractState,N<:AbstractPhase,Q<:AbstractInterface} = ConstantTVDomain(state,phase,interfaces,[phase.species[1].index,phase.species[end].index])
 export ConstantTVDomain
+
+
+function calcthermo!(d::ConstantTPDomain{Z,W,Y},y::T,t::Q) where {Z<:MolarState,W<:IdealGas,Y<:Integer,T<:AbstractArray,Q<:AbstractFloat}
+    d.state.ns = y[d.indexes[1]:d.indexes[2]]
+    d.state.t = t
+    d.state.N = sum(d.state.ns)
+    d.state.V = d.state.N*d.state.T*R/d.state.P
+    d.state.cs = d.state.ns./d.state.V
+    d.state.C = d.state.N/d.state.V
+end
+
+function calcthermo!(d::ConstantVDomain{Z,W,Y},y::T,t::Q) where {Z<:MolarState,W<:IdealGas,Y<:Integer,T<:AbstractArray,Q<:AbstractFloat}
+    d.state.ns = y[d.indexes[1]:d.indexes[2]]
+    d.state.T = y[d.indexes[3]]
+    d.state.t = t
+    d.state.N = sum(d.state.ns)
+    d.state.cs = d.state.ns./d.state.V
+    d.state.C = d.state.N/d.state.V
+    d.state.P = d.state.C*R*d.state.T
+    recalcgibbsandinternal!(d.phase,d.state)
+end
+
+function calcthermo!(d::ConstantTVDomain{Z,W,Y},y::T,t::Q) where {Z<:MolarState,W<:IdealDiluteSolution,Y<:Integer,T<:AbstractArray,Q<:AbstractFloat}
+    d.state.ns = y[d.indexes[1]:d.indexes[2]]
+    d.state.t = t
+    d.state.N = sum(d.state.ns)
+    d.state.cs = d.state.ns./d.state.V
+    d.state.C = d.state.N/d.state.V
+end
+export calcthermo!
+
+
 end
 export ConstantTVDomain 
