35e9e77fb9e097b3dd48a7a3b5308fca44962621
Matt Johnson <mjohnson541@gmail.com>
2018-10-28 21:11:45 -0400

    Divide domains into those in which rates are constant and those that aren't add and fill indexes property to track where that domain's variables (phase species moles and domain state variables) are stored in the overall dydt vector

--- rms/Domain.jl
+++ rms/Domain.jl
@@ -1,4 +1,5 @@
 using Parameters
+include("Constants.jl")
 include("Phase.jl")
 include("State.jl")
 include("Interface.jl")
@@ -6,23 +7,37 @@ include("Interface.jl")
 abstract type AbstractDomain end
 export AbstractDomain
 
-@with_kw mutable struct ConstantTPDomain{T<:AbstractState,N<:AbstractPhase,Q<:AbstractInterface} <: AbstractDomain
+abstract type AbstractConstantKDomain <: AbstractDomain  end
+export AbstractConstantKDomain
+
+abstract type AbstractVariableKDomain <: AbstractDomain end
+export AbstractVariableKDomain
+
+@with_kw struct ConstantTPDomain{T<:AbstractState,N<:AbstractPhase,S<:Integer} <: AbstractConstantKDomain
     state::T
     phase::N
-    iterfaces::Array{Q,1} = Array{EmptyInterface,1}()
+    interfaces::Array{AbstractInterface,1} = Array{AbstractInterface,1}()
+    indexes::Array{S,1}
 end
+ConstantTPDomain(;state::T,phase::N,interfaces::Array{Q,1}=Array{EmptyInterface,1}()) where {T<:AbstractState,N<:AbstractPhase,Q<:AbstractInterface} = ConstantTPDomain(state,phase,interfaces,[phase.species[1].index,phase.species[end].index])
 export ConstantTPDomain
 
-@with_kw mutable struct ConstantVDomain{T<:AbstractState,N<:AbstractPhase,Q<:AbstractInterface} <: AbstractDomain
+@with_kw struct ConstantVDomain{T<:AbstractState,N<:AbstractPhase,S<:Integer} <: AbstractVariableKDomain
     state::T
     phase::N
-    iterfaces::Array{Q,1} = Array{EmptyInterface,1}()
+    interfaces::Array{AbstractInterface,1} = Array{AbstractInterface,1}()
+    indexes::Array{S,1}
 end
+ConstantVDomain(;state::T,phase::N,interfaces::Array{Q,1}=Array{AbstractInterface,1}()) where {T<:AbstractState,N<:AbstractPhase,Q<:AbstractInterface} = ConstantVDomain(state,phase,interfaces,[phase.species[1].index,phase.species[end].index,0])
 export ConstantVDomain
 
-@with_kw mutable struct ConstantTVDomain{T<:AbstractState,N<:AbstractPhase,Q<:AbstractInterface} <: AbstractDomain
+@with_kw struct ConstantTVDomain{T<:AbstractState,N<:AbstractPhase,S<:Integer} <: AbstractConstantKDomain
     state::T
     phase::N
-    iterfaces::Array{Q,1} = Array{EmptyInterface,1}()
+    interfaces::Array{AbstractInterface,1} = Array{AbstractInterface,1}()
+    indexes::Array{S,1}
+end
+ConstantTVDomain(;state::T,phase::N,interfaces::Array{Q,1}=Array{EmptyInterface,1}()) where {T<:AbstractState,N<:AbstractPhase,Q<:AbstractInterface} = ConstantTVDomain(state,phase,interfaces,[phase.species[1].index,phase.species[end].index])
+export ConstantTVDomain
 end
 export ConstantTVDomain 
