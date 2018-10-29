01e83a822d68713be8c18995b1a994d6d735e601
Matt Johnson <mjohnson541@gmail.com>
2018-10-28 20:42:13 -0400

    add partial property variables to state object and make the tate molar rather than concentration

--- rms/State.jl
+++ rms/State.jl
@@ -1,17 +1,27 @@
 using Parameters
-
+import Base: length
 abstract type AbstractState end
 export AbstractState
 
 struct EmptyState <: AbstractState end
 export EmptyState
 
-@with_kw mutable struct ConcentrationState{T,Q,S,N,Z,J,Y<:AbstactFloat} <: AbstractState
-    cs::Array{T,1}
-    T::Q
-    P::S
-    V::N
-    C::J
-    t::Y
+@with_kw mutable struct MolarState{W<:AbstractFloat} <: AbstractState
+    ns::Array{W,1}
+    cs::Array{W,1} = Array{Float64,1}()
+    T::W = 0.0
+    P::W = 0.0
+    V::W = 0.0
+    C::W = 0.0
+    t::W = 0.0
+    N::W = 0.0
+    Gs::Array{W,1} = Array{Float64,1}()
+    Hs::Array{W,1} = Array{Float64,1}()
+    Us::Array{W,1} = Array{Float64,1}()
+    Ss::Array{W,1} = Array{Float64,1}()
+    Cps::Array{W,1} = Array{Float64,1}()
 end
-export ConcentrationState
+export MolarState
+
+length(m::MolarState) = 1
+export length
