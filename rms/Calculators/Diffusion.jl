cdbc1444f2cd82e4be273160ca41e6d764edcd57
Matt Johnson <mjohnson541@gmail.com>
2018-11-01 21:52:16 -0400

    remove unnecessary constructor and define where pi comes from

--- rms/Calculators/Diffusion.jl
+++ rms/Calculators/Diffusion.jl
@@ -17,8 +17,5 @@ export ConstantDiffusivity
 @with_kw struct StokesDiffusivity{N<:Number} <: AbstractDiffusivity
     r::N
 end
-
-StokesDiffusivity(;V::N=nothing) where {N<:Number} = StokesDiffusivity(r=(75.0*V/(pi*Na))^(1.0/3.0)/100.0)
-
-(sd::StokesDiffusivity)(;T::N,mu::Q,P::R=0.0) where {N,R,Q<:Number} = kB*T/(6*pi*mu*sd.r)
+(sd::StokesDiffusivity)(;T::N,mu::Q,P::R=0.0) where {N,R,Q<:Number} = kB*T/(6*Base.pi*mu*sd.r)
 export StokesDiffusivity
