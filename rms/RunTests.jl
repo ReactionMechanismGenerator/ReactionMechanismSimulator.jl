6187bb7783c3b8a097d686fe424e004dfa617914
Matt Johnson <mjohnson541@gmail.com>
2018-11-02 16:59:49 -0400

    add file for running all files starting with the name Test are

---
+++ rms/RunTests.jl
@@ -0,0 +1,21 @@
+"""
+Runs all files whose names start with "Test" in the current directory
+or any directories below it
+"""
+function runtests(path=".")
+    for (root,dirs,files) in walkdir(path)
+        for dir in dirs
+            runtests(joinpath(root,dir))
+        end
+        for file in files
+            if startswith(file,"Test")
+                println(joinpath(root,file))
+                include(joinpath(root,file))
+            end
+        end
+    end
+end
+
+runtests()
+
+@test true
