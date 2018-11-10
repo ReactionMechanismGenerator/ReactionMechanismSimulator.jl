889583802053b2df64c8c019c323d6121e09e6de
Matt Johnson <mjohnson541@gmail.com>
2018-11-09 22:48:26 -0500

    optimize rate coefficient calculation functions

--- rms/PhaseState.jl
+++ rms/PhaseState.jl
@@ -68,7 +68,7 @@ function recalcgibbsandinternal!(ph::IdealGas,st::MolarState)
 end
 export recalcgibbsandinternal!
 
-function getkf(rxn::ElementaryReaction,ph::T,st::MolarState) where {T<:AbstractPhase}
+@inline function getkf(rxn::ElementaryReaction,ph::T,st::MolarState) where {T<:AbstractPhase}
     return rxn.kinetics(T=st.T,P=st.P,C=st.C)
 end
 export getkf
@@ -80,25 +80,42 @@ for 2 spc calculates using the Smolchowski equation
 for >2 spc calculates using the Generalized Smolchowski equation
 Equations from Flegg 2016
 """
-function getDiffusiveRate(spcs::Q,st::MolarState) where {Q<:AbstractArray}
+@inline function getDiffusiveRate(spcs::Q,st::MolarState) where {Q<:AbstractArray}
     if length(spcs) == 1
         return Inf
     elseif length(spcs) == 2
-        kf = 4.0*Base.pi*(st.diffusivity[1]+st.diffusivity[2])*(spcs[1].radius+spcs[2].radius)*Na
+        @fastmath @inbounds kf = 4.0*Base.pi*(st.diffusivity[1]+st.diffusivity[2])*(spcs[1].radius+spcs[2].radius)*Na
     else
         N = length(spcs)
         a = (3.0*length(spcs)-5.0)/2.0
         Dinv = 1.0./st.diffusivity
         Dbar = 1.0./reverse(cumsum(Dinv))
         Dhat = st.diffusivity .+ Dbar
-        deltaN = sum(Dinv)/sum(sum([[1.0/(st.diffusivity[i]*st.diffusivity[m]) for m in 1:N-1 if i>m] for i in 2:N]))
-        kf = prod(Dhat[2:end].^1.5)*4*Base.pi^(a+1)/gamma(a)*(sum(getfield.(spcs,:radius))/sqrt(deltaN))^(2*a)*Na^(N-1)
+        @fastmath @inbounds deltaN = sum(Dinv)/sum(sum([[1.0/(st.diffusivity[i]*st.diffusivity[m]) for m in 1:N-1 if i>m] for i in 2:N]))
+        @fastmath @inbounds kf = prod(Dhat[2:end].^1.5)*4*Base.pi^(a+1)/gamma(a)*(sum(getfield.(spcs,:radius))/sqrt(deltaN))^(2*a)*Na^(N-1)
     end
     return kf
 end
 
-function getKc(rxn::ElementaryReaction,ph::T,st::MolarState) where {T<:AbstractPhase}
-    return exp(-(sum(st.Gs[rxn.productinds])-sum(st.Gs[rxn.reactantinds]))/(R*st.T))*(1.0e5/(R*st.T))^(length(rxn.productinds)-length(rxn.reactantinds))
+@inline function getKc(rxn::ElementaryReaction,ph::T,st::MolarState) where {T<:AbstractPhase}
+    Nreact = length(rxn.reactantinds)
+    Nprod = length(rxn.productinds)
+    dGrxn = 0.0
+    if Nreact == 1
+        @fastmath @inbounds dGrxn -= st.Gs[rxn.reactantinds[1]]
+    elseif Nreact == 2
+        @fastmath @inbounds dGrxn -= st.Gs[rxn.reactantinds[1]]+st.Gs[rxn.reactantinds[2]]
+    elseif Nreact == 3
+        @fastmath @inbounds dGrxn -= st.Gs[rxn.reactantinds[1]]+st.Gs[rxn.reactantinds[2]]+st.Gs[rxn.reactantinds[3]]
+    end
+    if Nprod == 1
+        @fastmath @inbounds dGrxn += st.Gs[rxn.productinds[1]]
+    elseif Nprod == 2
+        @fastmath @inbounds dGrxn += st.Gs[rxn.productinds[1]]+st.Gs[rxn.productinds[2]]
+    elseif Nprod == 3
+        @fastmath @inbounds dGrxn += st.Gs[rxn.productinds[1]]+st.Gs[rxn.productinds[2]]+st.Gs[rxn.productinds[3]]
+    end
+    return @inbounds @fastmath exp(-dGrxn/(R*st.T))*(1.0e5/(R*st.T))^(Nprod-Nreact)
 end
 export getKc
 
@@ -106,33 +123,33 @@ export getKc
 Calculates the forward and reverse rate coefficients for a given reaction, phase and state
 Maintains diffusion limitations if the phase has diffusionlimited=true
 """
-function getkfkrev(rxn::ElementaryReaction,ph::T,st::MolarState) where {T<:AbstractPhase}
+@inline function getkfkrev(rxn::ElementaryReaction,ph::T,st::MolarState) where {T<:AbstractPhase}
     kf = getkf(rxn,ph,st)
     Kc = getKc(rxn,ph,st)
-    krev = kf/Kc
+    @fastmath krev = kf/Kc
     if ph.diffusionlimited
         if length(rxn.reactants) == 1
             if length(rxn.products) > 1
                 krevdiff = getDiffusiveRate(rxn.products,st)
-                krev = krev*krevdiff/(krev+krevdiff)
-                kf = Kc*krev
+                @fastmath krev = krev*krevdiff/(krev+krevdiff)
+                @fastmath kf = Kc*krev
             end
         elseif length(rxn.products) == 1
             kfdiff = getDiffusiveRate(rxn.reactants,st)
-            kf = kf*kfdiff/(kf+kfdiff)
-            krev = kf/Kc
+            @fastmath kf = kf*kfdiff/(kf+kfdiff)
+            @fastmath krev = kf/Kc
         elseif length(rxn.products) == length(rxn.reactants)
             kfdiff = getDiffusiveRate(rxn.reactants,st)
             krevdiff = getDiffusiveRate(rxn.products,st)
-            kff = kf*kfdiff/(kf+kfdiff)
-            krevr = krev*krevdiff/(krev+krevdiff)
-            kfr = Kc*krevr
+            @fastmath kff = kf*kfdiff/(kf+kfdiff)
+            @fastmath krevr = krev*krevdiff/(krev+krevdiff)
+            @fastmath kfr = Kc*krevr
             if kff > kfr
                 kf = kfr
                 krev = krevr
             else
                 kf = kff
-                krev = kf/Kc
+                @fastmath krev = kf/Kc
             end
         end
     end
