f224a24f88de78b92f248f12f223fdc05c1c4aa1
Matt Johnson <mjohnson541@gmail.com>
2018-11-13 21:53:22 -0500

    create an EmptySolvent type

--- rms/Solvent.jl
+++ rms/Solvent.jl
@@ -4,6 +4,9 @@ include("Calculators.jl")
 abstract type AbstractSolvent end
 export AbstractSolvent
 
+struct EmptySolvent <: AbstractSolvent end
+export EmptySolvent
+
 struct Solvent <: AbstractSolvent
     name::String
     mu::AbstractViscosity
