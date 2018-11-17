6ee1297b0a060c78b91e63a98ec7e7e65ffde86b
Matt Johnson <mjohnson541@gmail.com>
2018-11-16 22:06:24 -0500

    add a function for calculating df_i/dp_j for f(x)=dy/dt for ConstantTPDomain and ConstantTVDomain

--- rms/Reactor.jl
+++ rms/Reactor.jl
@@ -66,3 +66,86 @@ function dydtBatchReactor!(y::Array{U,1},t::Z,domain::Q) where {Z<:Real,U<:Real,
     return dydt
 end
 export dydtBatchReactor!
+function ratederivative(;d::W,cs::Q,V::Y,T::Y2,kfs::Z,krevs::X,sparse::Bool=false) where {W<:Union{ConstantTPDomain,ConstantTVDomain},Q<:AbstractArray,Y2<:Real,Y<:Real,Z<:AbstractArray,X<:AbstractArray}
+    Nspcs = length(cs)
+    rxns = d.phase.reactions
+    Nrxns = length(rxns)
+
+    if sparse
+        ratederiv = spzeros(Nspcs,Nspcs+Nrxns)
+    else
+        ratederiv = zeros(Nspcs,Nspcs+Nrxns)
+    end
+    RTinv = 1.0/(R*T)
+
+    for (j,rxn) in enumerate(rxns)
+        Nreact = length(rxn.reactantinds)
+        Nprod = length(rxn.productinds)
+
+        if Nreact == 1
+            rind1 = rxn.reactantinds[1]
+            fderiv = cs[rind1]
+        elseif Nreact == 2
+            rind1,rind2 = rxn.reactantinds
+            fderiv = cs[rind1]*cs[rind2]
+        else
+            rind1,rind2,rind3 = rxn.reactantinds
+            fderiv = cs[rind1]*cs[rind2]*cs[rind3]
+        end
+
+        if Nprod == 1
+            pind1 = rxn.productinds[1]
+            rderiv = krevs[j]/kfs[j]*cs[pind1]
+        elseif Nprod == 2
+            pind1,pind2 = rxn.productinds
+            rderiv = krevs[j]/kfs[j]*cs[pind1]*cs[pind2]
+        else
+            pind1,pind2,pind3 = rxn.productinds
+        end
+
+        flux = fderiv-rderiv
+        gderiv = rderiv*kfs[j]*RTinv
+
+        deriv = zeros(Nspcs)
+
+        deriv[rind1] += gderiv
+        if Nreact > 1
+            deriv[rind2] += gderiv
+            if Nreact > 2
+                deriv[rind3] == gderiv
+            end
+        end
+
+        deriv[pind1] -= gderiv
+        if Nprod > 1
+            deriv[pind2] -= gderiv
+            if Nprod > 2
+                deriv[pind3] -= gderiv
+            end
+        end
+
+        ratederiv[rind1,j] -= flux
+        ratederiv[rind1,Nrxns+1:Nrxns+Nspcs] .-= deriv
+        if Nreact > 1
+            ratederiv[rind2,j] -= flux
+            ratederiv[rind2,Nrxns+1:Nrxns+Nspcs] .-= deriv
+            if Nreact > 2
+                ratederiv[rind3,j] -= flux
+                ratederiv[rind3,Nrxns+1:Nrxns+Nspcs] .-= deriv
+            end
+        end
+
+        ratederiv[pind1,j] += flux
+        ratederiv[pind1,Nrxns+1:Nrxns+Nspcs] .+= deriv
+        if Nprod > 1
+            ratederiv[pind2,j] += flux
+            ratederiv[pind2,Nrxns+1:Nrxns+Nspcs] .+= deriv
+            if Nprod > 2
+                ratederiv[pind3,j] += flux
+                ratederiv[pind3,Nrxns+1:Nrxns+Nspcs] .+= deriv
+            end
+        end
+    end
+    return V*ratederiv
+end
+export ratederivative
