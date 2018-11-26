8c96a54c7cef86dc5327a32d325cafdfcbe79260
Matt Johnson <mjohnson541@gmail.com>
2018-11-26 16:21:52 -0500

    add function for retrieving the reaction rates production and loss matrix

--- rms/Solution.jl
+++ rms/Solution.jl
@@ -46,3 +46,42 @@ getP(bsol::BatchSolution{Q,W,L,G}, t::K) where {W<:ConstantVDomain,K<:Real,Q,G,L
 getC(bsol::BatchSolution{Q,W,L,G}, t::K) where {W<:ConstantTPDomain,K<:Real,Q,G,L} = bsol.domain.P/(R*bsol.domain.T)
 getC(bsol::BatchSolution{Q,W,L,G}, t::K) where {W<:Union{ConstantVDomain,ConstantTVDomain},K<:Real,Q,G,L} = bsol.N(t)/bsol.domain.V
 
+"""
+calculates the rates of production/loss at a given time point
+this outputs a sparse matrix of  num reactions xnum species containing the production/loss
+rate of that species associated with that reaction
+"""
+function rops(bsol,t)
+    ropmat = spzeros(length(bsol.domain.phase.reactions),length(bsol.domain.phase.species))
+    xs = molefractions(bsol,t=t)
+    T = getT(bsol,t)
+    V = getV(bsol,t)
+    P = getP(bsol,t)
+    if :Gs in fieldnames(typeof(bsol.domain))
+        Gs = domain.Gs
+    else
+        Gs = calcgibbs(bsol.domain.phase,T)
+    end
+    if :solvent in fieldnames(typeof(bsol.domain.phase)) && typeof(bsol.domain.phase.solvent) != EmptySolvent
+        mu = phase.solvent.mu(T)
+    else
+        mu = 0.0
+    end
+    if bsol.domain.phase.diffusionlimited
+        diffs = getfield.(phase.species,:diffusion)(T=T,mu=mu,P=P)
+    else
+        diffs = Array{typeof(T),1}()
+    end
+    kfs,krevs = getkfkrevs(phase=bsol.domain.phase,T=T,P=P,C=1.0/V,N=1.0,ns=xs,Gs=Gs,diffs=diffs)
+    cs = xs./V
+    for (i,rxn) in enumerate(bsol.domain.phase.reactions)
+        R = getrate(rxn,cs,kfs,krevs)
+        for ind in rxn.reactantinds
+            ropmat[i,ind] = -R
+        end
+        for ind in rxn.productinds
+            ropmat[i,ind] = R
+        end
+    end
+    return ropmat
+end
