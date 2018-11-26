460cb342351f1eb08495758fba2204fee3f397ff
Matt Johnson <mjohnson541@gmail.com>
2018-11-26 16:16:13 -0500

    don't give the jacobian directly to to the ODEProblem
    doing so seems to cause issues in the solver currently, can be revisited later

--- rms/Reactor.jl
+++ rms/Reactor.jl
@@ -13,16 +13,9 @@ struct BatchReactor{D<:AbstractDomain}
     ode::ODEProblem
 end
 
-
 function BatchReactor(domain::T,y0::Array{W,1},tspan::Tuple) where {T<:AbstractDomain,W<:Real}
     dydt(y::Array{T,1},p::Nothing,t::Q) where {T<:Real,Q<:Real} = dydtBatchReactor!(y,t,domain)
-    if !domain.sensitivity
-        jac(J::Q,y::U,p::W,t::Z) where {Q<:AbstractArray,U<:AbstractArray,W,Z<:Real} = jacobianbatch!(J,y,p,t,domain)
-        odef = ODEFunction(dydt;jac=jac)
-        ode = ODEProblem(odef,y0,tspan)
-    else
-        ode = ODEProblem(dydt,y0,tspan)
-    end
+    ode = ODEProblem(dydt,y0,tspan)
     return BatchReactor(domain,ode)
 end
 export BatchReactor
