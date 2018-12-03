4ee0f6f6820e8cc101bf14a8875120f6c4327a31
Matt Johnson <mjohnson541@gmail.com>
2018-12-02 19:44:15 -0500

    add basic sensitivity analysis plots

--- src/Plotting.jl
+++ src/Plotting.jl
@@ -43,3 +43,55 @@ function plotmolefractions(bsol; tol=0.01)
 end
 
 export plotmolefractions
+
+function plotmaxthermosensitivity(bsol, spcname; N=0, tol= 1e-2)
+    spnames = getfield.(bsol.domain.phase.species,:name)
+    values = Array{Float64,1}()
+    outnames = Array{String,1}()
+    for spn in spnames
+        ind = argmax(abs.(getconcentrationsensitivity.(bsol,spcname,spn,bsol.sol.t)))
+        val = getconcentrationsensitivity(bsol,spcname,spn,bsol.sol.t[ind])
+        if abs(val)*4184.0 > tol
+            push!(values,val)
+            push!(outnames,spn)
+        end
+    end
+    inds = sortperm(abs.(values))
+    if N == 0
+        N = length(inds)
+    elseif N > length(inds)
+        N = length(inds)
+    end
+    inds = inds[1:N]
+    xs = Array{Float64,1}(1:length(inds))
+    barh(xs,values[inds].*4184.0)
+    yticks(xs,outnames[inds])
+    xlabel("dLn([$spcname])/d(G_i) mol/kcal")
+end
+export plotmaxthermosensitivity
+
+function plotmaxratesensitivity(bsol, spcname; N=0, tol= 1e-2)
+    Nrxns = length(bsol.domain.phase.reactions)
+    values = Array{Float64,1}()
+    outinds = Array{Int64,1}()
+    for i in 1:Nrxns
+        ind = argmax(abs.(getconcentrationsensitivity.(bsol,spcname,i,bsol.sol.t)))
+        val = getconcentrationsensitivity(bsol,spcname,i,bsol.sol.t[ind])
+        if abs(val) > tol
+            push!(values,val)
+            push!(outinds,i)
+        end
+    end
+    inds = sortperm(abs.(values))
+    if N == 0
+        N = length(inds)
+    elseif N > length(inds)
+        N = length(inds)
+    end
+    inds = inds[1:N]
+    xs = Array{Float64,1}(1:length(inds))
+    barh(xs,values[inds])
+    yticks(xs,getrxnstr.(bsol.domain.phase.reactions[outinds[inds]]))
+    xlabel("dLn([$spcname])/d(k_i) * k_i/[$spcname]")
+end
+export plotmaxratesensitivity
