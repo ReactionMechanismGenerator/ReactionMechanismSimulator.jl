54c9e6f9b2067950e5d61df3ce72b4c1cd18687a
Xiaorui Dong <xiaorui@mit.edu>
2019-07-24 23:33:39 -0400

    Clean up the unit test for Parse

--- src/TestParse.jl
+++ src/TestParse.jl
@@ -1,9 +1,11 @@
 using Test
-include("Parse.jl")
-out = readinput("../src/testing/superminimal.yml")
-r = out["gas"]["Reactions"][1]
+
+@testset "Read input from file" begin
+out = readinput("../src/testing/superminimal.rms")
+r = out["phase"]["Reactions"][1]
 s = r.products[1]
 
 @test r.kinetics(1000.0) ≈ 25618.8 rtol=1e-4
 @test getEnthalpy(s.thermo,1000.0) ≈ 20677.0 rtol=1e-4
-@test getEnthalpy(out["gas"]["Species"][5].thermo,1000.0) ≈ 20677.2 rtol=1e-4
+@test getEnthalpy(out["phase"]["Species"][5].thermo,1000.0) ≈ 20677.2 rtol=1e-4
+end;
