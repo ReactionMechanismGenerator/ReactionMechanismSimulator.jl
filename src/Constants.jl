32a1108bc207205f07274ff3d03454f561dc41c8
Xiaorui Dong <xiaorui@mit.edu>
2019-07-24 23:39:08 -0400

    Improve the accuracy of constants

--- src/Constants.jl
+++ src/Constants.jl
@@ -1,6 +1,6 @@
-const R = 8.314 #J/(mol*K)
+const R = 8.31446261815324 #J/(mol*K)
 export R
-const Na = 6.022e23 #molecules/mol
+const Na = 6.02214076e23 #molecules/mol
 export Na
 const kB = R/Na #J/(molecule*K)
 export kB
