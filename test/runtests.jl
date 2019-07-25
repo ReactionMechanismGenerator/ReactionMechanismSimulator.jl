9e6e93fb4f5074fb9010f09e32eec72b5c43c42a
Xiaorui Dong <xiaorui@mit.edu>
2019-07-24 23:37:29 -0400

    Clean up running all unit test code

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,12 @@
 using Test
-include("../src/RunTests.jl")
+include("../src/rmstest.jl")
+
+@testset "All Unit test for RMS" begin
+for (root,dirs,files) in walkdir("../src/")
+    for file in files
+        if startswith(file,"Test")
+            include(joinpath(root,file));
+        end
+    end
+end
+end;
