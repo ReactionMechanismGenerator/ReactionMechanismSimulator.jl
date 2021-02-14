c0bc2df35a14c40a7ef1f65d60d5d235fe65a149
Matt Johnson <mjohnson541@gmail.com>
2021-02-14 12:17:24 -0500

    add ReactiveInternalInterface
    this is an interface that is at the temperature and phi of domain1, which can change with respect to time

--- src/Interface.jl
+++ src/Interface.jl
@@ -1,4 +1,6 @@
 using LinearAlgebra
+using DiffEqBase
+using LsqFit
 
 abstract type AbstractInterface end
 export AbstractInterface
@@ -9,12 +11,60 @@ export AbstractBoundaryInterface
 abstract type AbstractInternalInterface <: AbstractInterface end
 export AbstractInternalInterface
 
+abstract type AbstractReactiveInternalInterface <: AbstractInternalInterface end
+export AbstractReactiveInternalInterface
+
 struct EmptyInterface <: AbstractInterface end
 export EmptyInterface
 
-@with_kw struct IdealGasCatalystInterface{T<:AbstractPhase,N<:AbstractPhase,Q<:AbstractReaction} <: AbstractInternalInterface
-    gas::T
-    catalyst::N
+struct ReactiveInternalInterface{T,B,C,C2,N,Q<:AbstractReaction,X} <: AbstractReactiveInternalInterface
+    domain1::T
+    domain2::N
+    reactions::Array{Q,1}
+    veckinetics::X
+    veckineticsinds::Array{Int64,1}
+    rxnarray::B
+    stoichmatrix::C
+    Nrp1::C2
+    Nrp2::C2
+    A::Float64
+    parameterindexes::Array{Int64,1}
+    domaininds::Array{Int64,1}
+    p::Array{Float64,1}
+    reversibililty::Array{Bool,1}
+end
+function ReactiveInternalInterface(domain1,domain2,reactions,A)
+    vectuple,vecinds,otherrxns,otherrxninds,posinds = getveckinetics(reactions)
+    rxns = vcat(reactions[vecinds],reactions[otherrxninds])
+    rxns = [ElementaryReaction(index=i,reactants=rxn.reactants,reactantinds=rxn.reactantinds,products=rxn.products,
+        productinds=rxn.productinds,kinetics=rxn.kinetics,radicalchange=rxn.radicalchange,reversible=rxn.reversible,pairs=rxn.pairs) for (i,rxn) in enumerate(rxns)]
+    rxnarray = getinterfacereactioninds(domain1,domain2,rxns)
+    M,Nrp1,Nrp2 = getstoichmatrix(domain1,domain2,reactions)
+    reversibility = Array{Bool,1}(getfield.(rxns,:reversible))
+    return ReactiveInternalInterface(domain1,domain2,
+            rxns,vectuple,posinds,rxnarray,M,Nrp1,Nrp2,A,[1,length(reactions)],
+            [0,1],ones(length(rxns)),reversibility),ones(length(rxns))
+end
+export ReactiveInternalInterface
+
+function getkfskrevs(ri::ReactiveInternalInterface,T1,T2,phi1,phi2,Gs1,Gs2,cstot::Array{Q,1}) where {Q}
+    kfs = getkfs(ri,T1,0.0,0.0,Array{Q,1}(),ri.A,phi1)
+    Kc = getKc.(ri.reactions,ri.domain1.phase,ri.domain2.phase,Ref(Gs1),Ref(Gs2),T1,phi1)
+    krevs = kfs./Kc
+    return kfs,krevs
+end
+
+function evaluate(ri::ReactiveInternalInterface,dydt,domains,T1,T2,phi1,phi2,Gs1,Gs2,cstot,p::W) where {W<:DiffEqBase.NullParameters}
+    kfs,krevs = getkfskrevs(ri,T1,T2,phi1,phi2,Gs1,Gs2,cstot)
+    addreactionratecontributions!(dydt,ri.rxnarray,cstot,kfs,krevs,ri.A)
+end
+
+function evaluate(ri::ReactiveInternalInterface,dydt,domains,T1,T2,phi1,phi2,Gs1,Gs2,cstot,p)
+    kfs,krevs = getkfskrevs(ri,T1,T2,phi1,phi2,Gs1,Gs2,cstot)
+    addreactionratecontributions!(dydt,ri.rxnarray,cstot,kfs.*p[ri.parameterindexes[1]:ri.parameterindexes[2]],krevs.*p[ri.parameterindexes[1]:ri.parameterindexes[2]],ri.A)
+end
+export evaluate
+
     reactions::Array{Q,1}
 """
 construct the stochiometric matrix for the reactions crossing both domains and the reaction molecule # change
