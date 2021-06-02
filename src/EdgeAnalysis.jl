36b4d3afa3da3a1a861f822b10a028364a4509fe
Matt Johnson <mjohnson541@gmail.com>
2021-06-02 13:20:22 -0400

    add Termination criteria objects

--- src/EdgeAnalysis.jl
+++ src/EdgeAnalysis.jl
@@ -2,3 +2,24 @@
 Tools for model edge analysis for automatic mechanism generation
 """
 
+using Logging
+using Sundials
+using SparseArrays
+using DiffEqBase: build_solution
+
+abstract type AbstractTerminationCriterion end
+
+struct TerminationTime <: AbstractTerminationCriterion
+    time::Float64
+end
+struct TerminationConversion <: AbstractTerminationCriterion
+    species::Species
+    conversion::Float64
+end
+struct TerminationRateRatio <: AbstractTerminationCriterion
+    ratio::Float64
+end
+
+export TerminationTime
+export TerminationConversion
+export TerminationRateRatio
