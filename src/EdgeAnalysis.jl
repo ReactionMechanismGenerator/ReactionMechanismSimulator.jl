5bfcc9659e1bb2dff6fe82edc3920e376d9f72bf
Matt Johnson <mjohnson541@gmail.com>
2021-06-02 13:37:58 -0400

    add code for generating Simulation/SystemSimulation objects for core and edge

--- src/EdgeAnalysis.jl
+++ src/EdgeAnalysis.jl
@@ -23,3 +23,48 @@ end
 export TerminationTime
 export TerminationConversion
 export TerminationRateRatio
+
+"""
+Generate appropriate core Simulation/SystemSimulation object
+"""
+function tosim(sol,domains,inters,p)
+    return Simulation(sol,domains,inters,p)
+end
+
+"""
+Generate appropriate core Simulation/SystemSimulation object
+"""
+function tosim(sol,domains::Tuple,inters,p)
+   return SystemSimulation(sol,domains,inters,p) 
+end
+
+"""
+Generate appropriate edge Simulation/SystemSimulation object
+"""
+function getsim(inte,react,coreedgedomain,inters,p,coretoedgespcmap)
+    ycoreedge = getycoreedge(inte.u,coretoedgespcmap,coreedgedomain.indexes[end])
+    sol = build_solution(react.ode,inte.alg,[0.0,inte.t],[ycoreedge,ycoreedge])
+    sim = Simulation(sol,coreedgedomain,inters,p)
+    return sim
+end
+
+"""
+Generate appropriate edge Simulation/SystemSimulation object
+"""
+function getsim(inte,react,coreedgedomains::Tuple,inters,p,coretoedgespcmap)
+    ycoreedge = getycoreedge(inte.u,coretoedgespcmap,coreedgedomains[end].indexes[end])
+    sol = build_solution(react.ode,inte.alg,[0.0,inte.t],[ycoreedge,ycoreedge])
+    ssys = SystemSimulation(sol,coreedgedomains,inters,p)
+    return ssys
+end
+
+"""
+Generate a state vector appropriate for both the edge and core from the core state vector
+"""
+function getycoreedge(y,coretoedgespcmap,numedgespc)
+    ycoreedge = zeros(numedgespc)
+    for (coreind,edgeind) in coretoedgespcmap
+        ycoreedge[edgeind] = y[coreind]
+    end
+    return ycoreedge
+end
