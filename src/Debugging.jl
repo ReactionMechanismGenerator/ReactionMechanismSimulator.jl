ecf5edac51230f0627251c64f56ae4109ae4c922
Matt Johnson <mjohnson541@gmail.com>
2021-09-29 15:59:43 -0400

    add structures for storing debugging information

--- src/Debugging.jl
+++ src/Debugging.jl
@@ -1,2 +1,30 @@
 using Parameters
 using Statistics
+
+@with_kw struct DebugSpecies
+    name::String
+    G298::Float64 = 0.0
+    ratio::Float64 = 0.0
+    dy::Float64 = 0.0
+    tol::Float64 = 0.0
+    index::Int64 = 0
+end
+
+@with_kw struct DebugReaction
+    reactants::Array{DebugSpecies,1}
+    products::Array{DebugSpecies,1}
+    rxnstring::String
+    tol::Float64 = 0.0
+    kf::Float64 = 0.0
+    krev::Float64 = 0.0
+    rt::Float64 = 0.0
+    ratio::Float64 = 0.0
+    T::Float64 = 0.0
+    P::Float64 = 0.0
+    index::Int64 = 0
+end
+
+struct DebugMech
+    spcs::Array{DebugSpecies,1}
+    rxns::Array{DebugReaction,1}
+end
