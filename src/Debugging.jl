2caf8d10a3f9abbd9a9f0460660c4cd3386a6862
Matt Johnson <mjohnson541@gmail.com>
2021-09-29 16:01:38 -0400

    add functions for generating species and reaction debug objects
    these contain information related to the fluxes at the crash point
    and the model parameters associated with these objects

--- src/Debugging.jl
+++ src/Debugging.jl
@@ -28,3 +28,18 @@ struct DebugMech
     spcs::Array{DebugSpecies,1}
     rxns::Array{DebugReaction,1}
 end
+
+function getdebugreaction(rxn::ElementaryReaction;tol=0.0,kf=0.0,krev=0.0,rt=0.0,ratio=0.0,T=0.0,P=0.0,index=0)
+    return DebugReaction([getdebugspecies(spc) for spc in rxn.reactants],
+            [getdebugspecies(spc) for spc in rxn.products],
+            getrxnstr(rxn),tol,kf,krev,rt,ratio,T,P,index)
+end
+
+export getdebugreaction
+
+function getdebugspecies(spc::Species;dy=0.0,ratio=0.0,tol=0.0,index=0)
+    G298 = getGibbs(spc.thermo,298.0)
+    return DebugSpecies(spc.name,G298,ratio,dy,tol,index)
+end
+
+export getdebugspecies
