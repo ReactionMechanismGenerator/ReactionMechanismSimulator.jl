8a3955058c225c29ded48e902f5e363f120a3559
Hao-Wei Pang <hwpang@mit.edu>
2023-10-17 22:18:20 -0400

    Test that we can parse rmg fragment mechanism

--- src/TestParse.jl
+++ src/TestParse.jl
@@ -9,3 +9,13 @@ s = r.products[1]
 @test getEnthalpy(s.thermo,1000.0) ≈ 20677.0 rtol=1e-4
 @test getEnthalpy(out["phase"]["Species"][5].thermo,1000.0) ≈ 20677.2 rtol=1e-4
 end;
+
+@testset "Read fragment input from file" begin
+    out = readinput("../src/testing/minimal_rmg_fragment.rms")
+    fragment = out["phase"]["Species"][5]
+
+    # check that fragment structure is parsed correctly and the representative molecule is assigned
+    @test fragment.atomnums["C"] == 18
+    @test fragment.molecularweight ≈ 0.25046 rtol=1e-4
+    @test fragment.radius ≈ 4.66375e-10 rtol=1e-4
+end;
