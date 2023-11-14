294f1eebe2c33c692dd9ff585645ee97e30edb8b
Anna Doner <47831564+donerancl@users.noreply.github.com>
2023-11-14 11:33:48 -0500

    Update Interface.jl
    added check if T is a function for LiquidVaporTransferInternalInterfaceConstantT

--- src/Interface.jl
+++ src/Interface.jl
@@ -392,8 +392,13 @@ function VaporLiquidMassTransferInternalInterfaceConstantT(domaingas,domainliq,i
     T = domainliq.T
     phase = domainliq.phase
     ignoremasstransferspcinds = getinterfaceignoremasstransferspcinds(domaingas,domainliq,ignoremasstransferspcnames)
-    kLAs = [kLA(T=T(0)) for kLA in getfield.(phase.species,:liquidvolumetricmasstransfercoefficient)]
-    kHs = [kH(T=T(0)) for kH in getfield.(phase.species,:henrylawconstant)]
+    if T isa Function 
+	kLAs = [kLA(T=T(0)) for kLA in getfield.(phase.species,:liquidvolumetricmasstransfercoefficient)]
+	kHs = [kH(T=T(0)) for kH in getfield.(phase.species,:henrylawconstant)]
+    else 
+	kLAs = [kLA(T=T) for kLA in getfield.(phase.species,:liquidvolumetricmasstransfercoefficient)]
+	kHs = [kH(T=T) for kH in getfield.(phase.species,:henrylawconstant)]
+    end
     return VaporLiquidMassTransferInternalInterfaceConstantT(domaingas,domainliq,ignoremasstransferspcnames,ignoremasstransferspcinds,kLAs,kHs,[1,length(domainliq.phase.species)],[0,0],ones(length(domainliq.phase.species))),ones(length(domainliq.phase.species))
 end
 export VaporLiquidMassTransferInternalInterfaceConstantT
