34549999d651d127c8da92b86870d031ceea4dde
Matt Johnson <mjohnson541@gmail.com>
2024-02-24 12:47:00 -0800

    optimize derivative function

--- src/ThreadedSensitivities.jl
+++ src/ThreadedSensitivities.jl
@@ -111,19 +111,21 @@ function generatesensitivityodes(react, sol)
         jacy!(J::Q2, y::T, p::V, t::Q) where {Q2,T,Q<:Real,V} = jacobiany!(J, y, p, t, r.domain, r.interfaces, nothing)
         jacp!(J::Q2, y::T, p::V, t::Q) where {Q2,T,Q<:Real,V} = jacobianp!(J, y, p, t, r.domain, r.interfaces, nothing)
 
-        function dsdt!(ds, s, local_params, t)
-            jy = zeros(length(y0), length(y0))
-            jp = zeros(length(y0), length(p))
-            y = sol(t)
-            jacy!(jy, y, p, t)
-            jacp!(jp, y, p, t)
-            @views @inbounds c = jp[:, i]
-            @inbounds ds .= jy * s .+ c
+        function dsdt(p_index)
+            function dsdt!(ds, s, local_params, t)
+                jy = zeros(length(y0), length(y0))
+                jp = zeros(length(y0), length(p))
+                jacy!(jy, sol(t), p, t)
+                jacp!(jp, sol(t), p, t)
+                c = jp[:, p_index]
+                ds .= jy*s .+ c
+            end
+            return dsdt!
         end
 
         # Create list of ODEProblems for each batch of parameters
 
-        odefcn = ODEFunction(dsdt!)
+        odefcn = ODEFunction(dsdt(i))
         prob = ODEProblem(odefcn, zeros(length(y0)), tspan, 0)
         push!(sa_list, prob)
     end
