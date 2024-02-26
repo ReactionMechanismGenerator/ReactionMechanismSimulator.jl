7de0cf411190e5ba01fb41f8ec7e6848c7a4b109
Matt Johnson <mjohnson541@gmail.com>
2024-02-26 08:14:44 -0800

    build order tweak

--- deps/build.jl
+++ deps/build.jl
@@ -4,8 +4,8 @@ if PyCall.pyversion.major != 3 || PyCall.pyversion.minor != 7
     const Pkg = Base.require(Base.PkgId(Base.UUID("44cfe95a-1eb2-52ea-b672-e2afdf69b78f"), "Pkg"))
     Conda.rm("mamba")
     Conda.add("conda=4")
-    Conda.add("python=3.7")
     Conda.add("mamba")
+    Conda.add("python=3.7")
     try
         Conda.rm("numpy") #get around MKL problem
     catch e 
