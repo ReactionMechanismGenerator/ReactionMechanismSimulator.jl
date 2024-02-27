53e1ab627acebf4c225e0a0093cf0c8fedcce8ec
Matt Johnson <mjohnson541@gmail.com>
2024-02-26 22:01:09 -0800

    use python 3.7

--- deps/build.jl
+++ deps/build.jl
@@ -1,9 +1,13 @@
 using PyCall
 using Conda
 packages = Conda._installed_packages()
-if !("rmg" in packages) && !("rmgmolecule" in packages) && (PyCall.pyversion.major != 3 || PyCall.pyversion.minor != 8)
+if !("rmg" in packages) && !("rmgmolecule" in packages) && (PyCall.pyversion.major != 3 || PyCall.pyversion.minor != 7)
     const Pkg = Base.require(Base.PkgId(Base.UUID("44cfe95a-1eb2-52ea-b672-e2afdf69b78f"), "Pkg"))
-    Conda.add("python=3.8")
+    Conda.rm("mamba")
+    Conda.add("conda=4")
+    Conda.add("mamba")
+    Conda.update()
+    Conda.add("python=3.7")
     try
         Conda.rm("numpy") #get around MKL problem
     catch e 
