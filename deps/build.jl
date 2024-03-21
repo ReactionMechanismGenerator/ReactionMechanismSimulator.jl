3f105ab49f2ba1cddbb1c7483bf4ea39c101f963
Hao-Wei Pang <hwpang@mit.edu>
2024-03-21 18:06:12 -0400

    update

--- deps/build.jl
+++ deps/build.jl
@@ -1,12 +1,20 @@
 using CondaPkg
 using PythonCall
 packages = keys(CondaPkg.current_packages())
-if !("rmg" in packages) && !("rmgmolecule" in packages) || !(v"3.7" <= PythonCall.C.python_version() && PythonCall.C.python_version() <= v"3.9")
-    const Pkg = Base.require(Base.PkgId(Base.UUID("44cfe95a-1eb2-52ea-b672-e2afdf69b78f"), "Pkg"))
-    CondaPkg.add("python"; version="3.9")
-    CondaPkg.add("matplotlib")
-    CondaPkg.add("rdkit")
-    CondaPkg.add("pydot")
+if !("rmg" in packages) && !("rmgmolecule" in packages)
     CondaPkg.add("rmgmolecule"; version=">=0.3.0", channel="mjohnson541")
-    Pkg.build("PythonCall")
 end
+if !(v"3.7" <= PythonCall.C.python_version() && PythonCall.C.python_version() <= v"3.9")
+    CondaPkg.add("python"; version="3.9")
+end
+if !("matplotlib" in packages)
+    CondaPkg.add("matplotlib")
+end
+if !("rdkit" in packages)
+    CondaPkg.add("rdkit")
+end
+if !("pydot" in packages)
+    CondaPkg.add("pydot")
+end
+const Pkg = Base.require(Base.PkgId(Base.UUID("44cfe95a-1eb2-52ea-b672-e2afdf69b78f"), "Pkg"))
+Pkg.build("PythonCall")
