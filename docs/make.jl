b4a3fe868dbc3328fc8255885f4fe99f8359ccfa
Hao-Wei Pang <hwpang@mit.edu>
2024-03-21 21:41:08 -0400

    update

--- docs/make.jl
+++ docs/make.jl
@@ -1,27 +1,6 @@
 import Logging
 Logging.disable_logging(Logging.Warn)
 
-using CondaPkg
-using PythonCall
-packages = keys(CondaPkg.current_packages())
-if !("rmg" in packages) && !("rmgmolecule" in packages)
-    CondaPkg.add("rmgmolecule"; version=">=0.3.0", channel="mjohnson541")
-end
-if !(v"3.7" <= PythonCall.C.python_version() && PythonCall.C.python_version() <= v"3.9")
-    CondaPkg.add("python"; version="3.9")
-end
-if !("matplotlib" in packages)
-    CondaPkg.add("matplotlib")
-end
-if !("rdkit" in packages)
-    CondaPkg.add("rdkit")
-end
-if !("pydot" in packages)
-    CondaPkg.add("pydot")
-end
-const Pkg = Base.require(Base.PkgId(Base.UUID("44cfe95a-1eb2-52ea-b672-e2afdf69b78f"), "Pkg"))
-Pkg.build("PythonCall")
-
 using Documenter, ReactionMechanismSimulator
 
 makedocs(
