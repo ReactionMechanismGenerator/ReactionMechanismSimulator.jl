4d8f6a480c57836707fd81d23b478f76c357a373
Hao-Wei Pang <hwpang@mit.edu>
2024-06-23 17:58:55 -0400

    Update build to use CondaPkg

--- deps/build.jl
+++ deps/build.jl
@@ -1,21 +1,16 @@
-using PyCall
-using Conda
-packages = Conda._installed_packages()
-if !("rmg" in packages) && !("rmgmolecule" in packages) && (PyCall.pyversion.major != 3 || PyCall.pyversion.minor != 7)
-    const Pkg = Base.require(Base.PkgId(Base.UUID("44cfe95a-1eb2-52ea-b672-e2afdf69b78f"), "Pkg"))
-    Conda.rm("mamba")
-    Conda.add("conda=4")
-    Conda.add("mamba")
-    Conda.update()
-    Conda.add("python=3.7")
-    try
-        Conda.rm("numpy") #get around MKL problem
-    catch e 
+using CondaPkg
+using PythonCall
+packages = keys(CondaPkg.current_packages())
+if !("rmg" in packages) && !("rmgmolecule" in packages)
+
+    if !(v"3.7" <= PythonCall.C.python_version() && PythonCall.C.python_version() <= v"3.9")
+        CondaPkg.add("python"; version="3.9")
     end
-    Conda.add("nomkl")
-    Conda.add("numpy")
-    Conda.add_channel("mjohnson541")
-    Conda.add_channel("hwpang")
-    Conda.add("rmgmolecule")
-    Pkg.build("PyCall")
+    CondaPkg.add("rmgmolecule"; version=">=0.3.0", channel="mjohnson541")
+    CondaPkg.add("matplotlib", channel="conda-forge")
+    CondaPkg.add("rdkit", channel="conda-forge")
+    CondaPkg.add("pydot", channel="conda-forge")
 end
+
+const Pkg = Base.require(Base.PkgId(Base.UUID("44cfe95a-1eb2-52ea-b672-e2afdf69b78f"), "Pkg"))
+Pkg.build("PythonCall")
